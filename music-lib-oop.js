ec7923243be01f2c6e4cc3f7ffdbe32e864075b9
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-09 22:39:51 +0000

    Can create tracks in a playlist

--- music-lib-oop.js
+++ music-lib-oop.js
@@ -4,20 +4,47 @@ function Library (name, creator) {
   this.playlists = [];
 }
 
+
 Library.prototype.addPlaylist = function (name) {
 
   let playlist = {
-    name: name
+    name: name,
+    tracks: []
+
   };
   this.playlists.push(playlist);
-
 }
 
-const lib = new Library("theLib", "me");
+
+Library.prototype.addTrackToPlaylist = function (playlistName, title, rating, length) {
+  if (typeof rating !== 'number' || rating > 5 || rating < 1) {
+    console.log('Please input a valid rating');
+    return;
+  }
+
+  let track = {
+    title: title,
+    rating: rating,
+    length: length
+  };
+
+  for (let playlist of this.playlists) {
+    if (playlist.name === playlistName) {
+      playlist.tracks.push(track);
+    } else {
+      console.log('Playlist does not exist');
+      return;
+    }
+  }
+}
+
+const lib = new Library('theLib', 'me');
 
 
-lib.addPlaylist("thePlaylist");
+lib.addPlaylist('thePlaylist');
+
+lib.addTrackToPlaylist('thePlaylist', 'Magic', 5, 1234);
+lib.addTrackToPlaylist('thePlaylist', 'Gold', 4, 4321);
 
 
-
-console.log(lib.playlists);
+console.log(lib.playlists[0].tracks[0]);
